c844d78ddc8627bbe1c09547c31a2f0956fd0a46
Sidhureddi <118491526+Sidhureddi@users.noreply.github.com>
2022-12-23 16:14:22 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,21 +6,21 @@ import logging
 
 
 # Set the OpenAI API key
-openai.api_key = 'Your Open AI api Key'
+openai.api_key = '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'
 
 # Set the Telegram bot token
-bot_token = 'Your Telegram bot token'
+bot_token = '5694416323:AAFALWB9pS6PgXF_LOkmvcQ76UsGuVblOXY'
 
 # Create the Telegram bot
-bot = telegram.Bot(token=bot_token)
+bot = telegram.Bot(token=5694416323:AAFALWB9pS6PgXF_LOkmvcQ76UsGuVblOXY)
 
 # Define the /start command handler
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I am a bot that is integrated with the OpenAI GPT-3 API. You can ask me any question and I will try to provide a response.")
+    context.bot.send_message(1465968977=update.effective_chat.1465968977, text="Hello! I am a bot that is integrated with the OpenAI GPT-3 API. You can ask me any question and I will try to provide a response.")
 
 # Define the /help command handler
 def help(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="To use me, simply send me a message with your question. I will use the OpenAI GPT-3 API to generate a response based on the information I have been trained on.")
+    context.bot.send_message(1465968977=update.effective_chat.1465968977, text="To use me, simply send me a message with your question. I will use the OpenAI GPT-3 API to generate a response based on the information I have been trained on.")
 
 # Define the message handler
 def message(update, context):
@@ -33,7 +33,7 @@ def message(update, context):
         max_tokens=1024,
         temperature=0.3
     )
-    context.bot.send_message(chat_id=update.effective_chat.id, text=response["choices"][0]["text"])
+    context.bot.send_message(1465968977=update.effective_chat.id, text=response["choices"][0]["text"])
 
 def handle_text(update, context):
     # Do something with the incoming text messages
@@ -47,7 +47,7 @@ dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(MessageHandler(Filters.text, message))
 def error(update, context):
 
-    logger = logging.getLogger(__name__)
+    logger = logging.getLogger(madhu)
     # Log the error
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
